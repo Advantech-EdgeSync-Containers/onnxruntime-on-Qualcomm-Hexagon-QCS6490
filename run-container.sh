57b842192fc19e33e1a83220db582dea3df913f0
yupooooo <47267289+gacky1601@users.noreply.github.com>
2026-02-23 18:08:40 +0800

    typo: rename container name in run-container.sh

--- run-container.sh
+++ run-container.sh
@@ -5,7 +5,7 @@ echo "Connecting to container..."
 
 # 2. Enter the container
 # When you type 'exit' or press Ctrl+D, the script continues to the next line
-docker exec -it qualcomm-onnxruntime-qnn-ready-container bash
+docker exec -it onnxruntime-on-qualcomm-hexagon-qcs6490 bash
 
 # 3. Cleanup after exiting the bash session
 echo "Exited container. Cleaning up..."
